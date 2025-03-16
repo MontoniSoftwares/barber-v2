c3338da44f81b143148060aa0b4efecd17fb121e
Roberto <robertomontoni.dev@gmail.com>
2025-03-16 00:30:40 -0300

    chore add Implantação de Logs

--- app/_lib/prisma.ts
+++ app/_lib/prisma.ts
@@ -7,10 +7,14 @@ declare global {
 
 let prisma: PrismaClient
 if (process.env.NODE_ENV === "production") {
+  console.log("Inicializando Prisma Client em produção...")
   prisma = new PrismaClient()
+  console.log("Prisma Client inicializado em produção.")
 } else {
   if (!global.cachedPrisma) {
+    console.log("Inicializando Prisma Client em desenvolvimento...")
     global.cachedPrisma = new PrismaClient()
+    console.log("Prisma Client inicializado em desenvolvimento.")
   }
   prisma = global.cachedPrisma
 }
